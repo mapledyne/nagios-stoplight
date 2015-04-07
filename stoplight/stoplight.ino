af48891620e286ca63eb2f98bd23818fc590d542
Michael Knowles <mknowles@fulcrum.net>
2015-04-07 10:41:24 -0700

    Initial commit

---
+++ stoplight/stoplight.ino
@@ -0,0 +1,123 @@
+#define REDPIN     5
+#define BLUEPIN    6
+#define GREENPIN   3
+
+#define MAXCOLOR    255
+#define NUMCOLORS   3
+
+int black[3]  = { 0, 0, 0 };
+int white[3]  = { 255, 255, 255 };
+int red[3]    = { 255, 0, 0 };
+int green[3]  = { 0, 255, 0 };
+int blue[3]   = { 0, 0, 255 };
+int yellow[3] = { 255, 255, 0 };
+int dimWhite[3] = { 50, 50, 50 };
+
+
+int currentColor[3] = { 0, 0, 0 };
+int color[3] = { 0, 0, 0 };
+int currentDirection = 1;
+int speed = 10;
+long time = 0;
+
+
+void setup() 
+{
+    pinMode(REDPIN, OUTPUT);
+    pinMode(GREENPIN, OUTPUT);
+    pinMode(BLUEPIN, OUTPUT);
+    Serial.begin(9600);
+    setColor(yellow);
+}
+
+void loop()
+{
+    checkMessages();
+    checkFade();
+}
+
+bool colorCompare(int col1[NUMCOLORS], int col2[NUMCOLORS])
+{
+    for (int x=0; x<NUMCOLORS; x++)
+    {
+        if (col1[x]!=col2[x])
+            return false;
+    }
+    return true;
+}
+
+void setColor(int newColor[NUMCOLORS])
+{
+    for( int x = 0; x < NUMCOLORS; x++ )
+    {
+        color[x] = newColor[x];
+    }
+    currentDirection = -1;
+}
+
+void drawColor(int newColor[NUMCOLORS])
+{
+    analogWrite(REDPIN, newColor[0]);
+    analogWrite(GREENPIN, newColor[1]);      
+    analogWrite(BLUEPIN, newColor[2]); 
+}
+
+void checkMessages()
+{
+    if(Serial.available() > 0)
+    {
+        char d = Serial.read();
+        Serial.println(d);
+        switch (d)
+        {
+            case 'R':
+                setColor(red);
+                break;
+            case 'G':
+                setColor(green);
+                break;
+            case 'B':
+                setColor(blue);
+                break;
+            case 'Y':
+                setColor(yellow);
+                break;
+            case 'W':
+                setColor(white);
+                break;
+            default:
+            break;
+        }
+    }
+}
+
+void checkFade()
+{
+    if (millis() < time + speed)
+        return;
+    
+    for ( int x = 0; x < NUMCOLORS; x++)
+    {
+       if (currentDirection>0)
+       {
+           if (currentColor[x]<color[x])
+           {
+               currentColor[x]+=currentDirection;
+           }
+       } else {
+           if (currentColor[x]>0)
+           {
+               currentColor[x]+=currentDirection;
+           }
+       }  
+    }
+    if (colorCompare(color,currentColor) || colorCompare(black, currentColor))
+    {
+        currentDirection=-currentDirection;
+    }
+    
+    drawColor(currentColor);
+    time = millis();
+    
+}
+
